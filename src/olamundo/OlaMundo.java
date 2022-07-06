eb2feea03308fdae180e17d120f25d913088db68
Mah <marinalarissacarpesrohrig@gmail.com>
2022-07-05 21:31:18 -0300

    PrimeiroCommit1.0

---
+++ src/olamundo/OlaMundo.java
@@ -0,0 +1,9 @@
+// Aula do dia 05/07/22 do curso FuturoDEV do Senai
+// Autor: Marina Larissa Carpes Röhrig
+package olamundo;
+
+public class OlaMundo {
+    public static void main(String[] args){
+
+    }
+}
